39636727e4e349023631034a4b8ecc46991fff63
KevinBaoB <kevinbaobui@gmail.com>
2022-06-20 13:44:09 -0500

    Replaced images with evolution

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById("digivolve").onclick = function () {
+  document.getElementById("Koromon").src =
+    "https://digimon.shadowsmith.com/img/agumon.jpg";
+  document.getElementById("Tsunomon").src =
+    "https://digimon.shadowsmith.com/img/gabumon.jpg";
+  document.getElementById("Yokomon").src =
+    "https://digimon.shadowsmith.com/img/biyomon.jpg";
+};
